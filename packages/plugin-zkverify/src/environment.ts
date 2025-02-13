a87a5d4beda802e4ae3c06eb868bb28613c7e197
AndreaRettaroli <andrea.rettaroli@studio.unibo.it>
2025-02-14 00:14:14 +0100

    perf: refactor

--- packages/plugin-zkverify/src/environment.ts
+++ packages/plugin-zkverify/src/environment.ts
@@ -2,28 +2,27 @@ import { IAgentRuntime } from "@elizaos/core";
 import { z } from "zod";
 
 export const zkVerifyEnvSchema = z.object({
-    ZKVERIFY_SIGNER_PK: z.string().min(1, "zkVerify signer key"),
+  ZKVERIFY_SIGNER_PK: z.string().min(1, "zkVerify signer key"),
 });
 
 export type zkVerifyConfig = z.infer<typeof zkVerifyEnvSchema>;
 
 export async function validateZKVerifyConfig(
-    runtime: IAgentRuntime
+  runtime: IAgentRuntime
 ): Promise<zkVerifyConfig> {
-    try {
-        const config = {
-            ZKVERIFY_SIGNER_PK: runtime.getSetting("ZKVERIFY_SIGNER_PK"),
-        };
-        console.log("config: ", config);
-        return zkVerifyEnvSchema.parse(config);
-    } catch (error) {
-        console.log("error::::", error);
-        if (error instanceof z.ZodError) {
-            const errorMessages = error.errors
-                .map((err) => `${err.path.join(".")}: ${err.message}`)
-                .join("\n");
-            throw new Error(`ZK verify validation failed:\n${errorMessages}`);
-        }
-        throw error;
+  try {
+    const config = {
+      ZKVERIFY_SIGNER_PK: runtime.getSetting("ZKVERIFY_SIGNER_PK"),
+    };
+    return zkVerifyEnvSchema.parse(config);
+  } catch (error) {
+    console.log("error::::", error);
+    if (error instanceof z.ZodError) {
+      const errorMessages = error.errors
+        .map((err) => `${err.path.join(".")}: ${err.message}`)
+        .join("\n");
+      throw new Error(`ZK verify validation failed:\n${errorMessages}`);
     }
+    throw error;
+  }
 }
